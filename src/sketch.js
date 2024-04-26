94322fe827a6b49b4d8fd889908896a6823a9703
Elton Zeng <poptako9@gmail.com>
2024-04-25 23:12:34 -0700

    main/image filters and key presses to change the filters.

--- src/sketch.js
+++ src/sketch.js
@@ -1,15 +1,24 @@
 //ascii based on video from Daniel Shiffman/thecoding train 
-const density = 'Ñ@#W$9876543210?!abc;:+=-,._    ';
-//const density = "abcdefghijklmnopqrstuvwxyz1234"
+const density = 'Ñ@#W$9876543210?!abc;:+=-,._ ';
 let img;
+let txt; 
+let slogans;
+let asciiFlag, pixelFlag, asciiNoise
+
+function preload() {
+    slogans = loadStrings('txt/slogans.txt')
+}
 
 //add in user input
-
 //file input from gpt and asking how to make an image take input 
 function setup(){
     createCanvas(600, 600)
     fileInput = createFileInput(handleFile);
-    fileInput.position(20, 20);
+    fileInput.position(20, 620);
+
+    button = createButton('save');
+    button.position(20, 650);
+    button.mousePressed(goSave);
 }
 
 function handleFile(file) {
@@ -25,9 +34,10 @@ function handleFile(file) {
 
 function draw(){
     background(0);
+    //ascii conversion code from the coding train link: https://www.youtube.com/watch?v=55iwMYv8tGI 
     if(img){
         //image(img, 0, 0, width, height)
-        img.resize(48, 48);
+        img.resize(64, 64);
         let w = width / img.width;
         let h = height/ img.height;
         img.loadPixels(); 
@@ -40,33 +50,49 @@ function draw(){
                 const avg = (r + g + b) / 3
                 //the third index is transparency
                 noStroke();
-                //fill(r, g, b)
-                //simple grayscale filter if you just divide instead 
-                //fill(avg)
+                //ascii 
+                //noise 
+                //add in the filters: 
+                //full color = c
                 fill(255)
+                if(keyIsPressed == true){
+                    switch(keyCode){
+                    //full color (b)
+                    case 66:
+                        fill(r, g, b)
+                        break
+                    //greyscale (g)
+                    case 71:
+                        fill(avg)
+                        break 
+                    //white (w)
+                    case 87:
+                        fill(255)
+                        break  
+                    default:
+                        fill(255)
+                        break;
+                    }
+                }
                 const len = density.length;
                 const charIndex = floor(map(avg, 0, 255, len, 0))
                 //square(i * w, j * h, w)   
                 //textSize(30)
+                //asked gpt to give me a list of slogans 
+                let chooseLine = random(slogans)
+                //textAlign(CENTER, CENTER)
+                //text(chooseLine, i * w, j * h)
                 text(density.charAt(charIndex), i * w, j * h);
             }
         }
-
     }
+    }
+//}
+
+function goSave(){
+    saveCanvas('image_filter', 'jpg')
 }
 
-function switchFilter(image){
-    switch(filter){
-        //based on the one by daniel shiffman
-        case "ascii":
-        case "sepia":
-        case "pixelate": 
-        case "oil":
-        default: 
-    }
-}
-
-//turn the image into more of a 40 by 40 image instead 
-function descale(image){
+function imageFilter(){
 
 }
